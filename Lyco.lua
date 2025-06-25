a6737ef55ddb43824b9549096a6260954318f249
qkdr <kskwkodh@gmail.com>
2025-06-25 10:12:01 +0300

    Lyco.lua

--- Lyco.lua
+++ Lyco.lua
@@ -1,1 +1,11 @@
+local games = {
+    [15438487904] = "https://raw.githubusercontent.com/qkdr/Lord/refs/heads/main/Thronix1.lua"
+}
 
+local defaultScript = "https://raw.githubusercontent.com/qkdr/Thronix/main/Thronix.lua"
+
+local scriptURL = games[game.PlaceId] or defaultScript
+
+getgenv().uiSize = UDim2.fromOffset(580, 400)
+
+loadstring(game:HttpGet(scriptURL))()
